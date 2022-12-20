130616d9df107a165ca4c4a444b146f133e177cb
Oleksandr Zhurba <a.zhurba0810@gmail.com>
2022-12-20 20:47:58 +0200

    refactor

--- js/main.js
+++ js/main.js
@@ -1,73 +1,124 @@
-const open=document.getElementById('open');
-const close=document.getElementById('close');
-const container=document.querySelector('.container');
-let q=0
-const cardsContainer=document.getElementById('cards-container');
-const prevBtn=document.getElementById('prev');
-const nextBtn=document.getElementById('next');
-const currentEl=document.getElementById('current');
-const showBtn=document.getElementById('show');
-const hideBtn=document.getElementById('hide');
-let questionEl=document.getElementById('question');
-let answerEl=document.getElementById('answer');
-const addCardBtn=document.getElementById('add-card');
-const clearBtn=document.getElementById('clear');
-const addContainer=document.getElementById('add-container');
-const cards=document.querySelector('.cards');
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const container = document.querySelector('.container');
+let q = 0
+const cardsContainer = document.getElementById('cards-container');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const currentEl = document.getElementById('current');
+const showBtn = document.getElementById('show');
+const hideBtn = document.getElementById('hide');
+let questionEl = document.getElementById('question');
+let answerEl = document.getElementById('answer');
+const addCardBtn = document.getElementById('add-card');
+const clearBtn = document.getElementById('clear');
+const addContainer = document.getElementById('add-container');
+const cards = document.querySelector('.cards');
+
+const myCards = [
+    {
+        description : "The simplest training site created on the classic react-based free movie API",
+        name: "Movie",
+        linkGit: "https://github.com/Shrek1891/movie2",
+        linkShow: "https://movie2-vert.vercel.app"
+    },
+    {
+        description : "The simplest training SPA created on the classic react-based free food API",
+        name: "Food SPA",
+        linkGit: "https://github.com/Shrek1891/food-spa",
+        linkShow: "https://food-spa-pi.vercel.app"
+    },
+    {
+        description : "The simplest training dashboard created on react with redux",
+        name: "Dashboard",
+        linkGit: "https://github.com/Shrek1891/dashbord-with-react",
+        linkShow: "https://dashbord-with-react.vercel.app"
+    },
+    {
+        description : "The simplest APP created on react with asynchronous redux",
+        name: "Flags",
+        linkGit: "https://github.com/Shrek1891/countries-flags",
+        linkShow: "https://countries-flags-omega.vercel.app"
+    }
+]
+
+myCards.forEach( (myCard, index) => {
+    const cardEl = document.createElement("div");
+    if (index == 0 ) {
+        cardEl.classList.add("active");
+    }
+    cardEl.innerHTML = `
+     <div class="inner-card">
+                    <div class="inner-card-front">
+                        <p>
+                            ${myCard.description}
+                            <br><br>
+                            <span class="blink">click me to get link</span>
+                        </p>
+                    </div>
+                    <div class="inner-card-back">
+                        <p>
+                            <a class="github" href=${myCard.linkGit} target="_blank"> github link </a><br/>
+                            <a class="vercel" href=${myCard.linkShow} target="_blank"> vercel link </a>
+                        </p>
+                    </div>
+                </div>
+`
+    cardEl.classList.add("card");
+    cardsContainer.appendChild(cardEl);
+})
 
 
-open.addEventListener("click",()=>{
+
+
+open.addEventListener("click", () => {
     container.classList.add('show-nav');
 })
 
 
-close.addEventListener("click",()=>{
+close.addEventListener("click", () => {
     container.classList.remove('show-nav');
 })
 
 
+let currentActiveCard = 0;
+const cardsEl = document.querySelectorAll('.card');
 
-currentActiveCard=0;
-const cardsEl=document.querySelectorAll('.card');
 //create a single card in DOM
 
 
 function updateCurrentText() {
-    currentEl.innerText=`${currentActiveCard+1}/${cardsEl.length}`;
+    currentEl.innerText = `${currentActiveCard + 1}/${myCards.length}`;
 }
 
 
-
-
-cards.addEventListener('click',()=>{
+cards.addEventListener('click', () => {
     cards.children[currentActiveCard].classList.toggle('show-answer');
 });
 
 
+nextBtn.addEventListener('click', () => {
+    cardsEl[currentActiveCard].className = 'card left';
 
+    currentActiveCard = currentActiveCard + 1;
 
-nextBtn.addEventListener('click',()=> {
-    cardsEl[currentActiveCard].className='card left';
-
-    currentActiveCard=currentActiveCard+1;
-
-    if (currentActiveCard>cardsEl.length-1) {
-        currentActiveCard=cardsEl.length-1;
+    if (currentActiveCard > cardsEl.length - 1) {
+        currentActiveCard = cardsEl.length - 1;
     }
-    cardsEl[currentActiveCard].className='card active';
+    cardsEl[currentActiveCard].className = 'card active';
 
     updateCurrentText();
 })
 
-prevBtn.addEventListener('click',()=> {
-    cardsEl[currentActiveCard].className='card right';
+prevBtn.addEventListener('click', () => {
+    cardsEl[currentActiveCard].className = 'card right';
 
-    currentActiveCard=currentActiveCard-1;
+    currentActiveCard = currentActiveCard - 1;
 
-    if (currentActiveCard<0) {
-        currentActiveCard=0;
+    if (currentActiveCard < 0) {
+        currentActiveCard = 0;
     }
-    cardsEl[currentActiveCard].className='card active';
+    cardsEl[currentActiveCard].className = 'card active';
 
     updateCurrentText();
 });
